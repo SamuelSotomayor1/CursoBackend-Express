a9978ee1c410efd67db51221890f40c16df724c2
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-09-08 03:50:02 -0300

    Se utiliza app.get

--- myApp.js
+++ myApp.js
@@ -1,6 +1,21 @@
 let express = require('express');
 let app = express();
 
+//console.log("Hello World")
+
+//app.get("/", function(req, res) {
+//  res.sendFile("Hello Express");
+//});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+})
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"});
+});
 
 
 
